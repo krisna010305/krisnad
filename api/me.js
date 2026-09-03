cd502029c0d5aa078888b27b6f07c846300326a7
krisna010305 <krisnadila192022@gmail.com>
2026-09-03 17:30:43 +0700

    Delete api directory

--- api/me.js
+++
@@ -1,6 +0,0 @@
-const { auth, safeUser, json } = require('./_common');
-module.exports = async (req, res) => {
-  const user = auth(req);
-  if (!user) return json(res, 401, { error: 'Sesi login tidak valid atau sudah kedaluwarsa.' });
-  return json(res, 200, { user: safeUser(user) });
-};
